27bc57f2bf369b68135793bea377d5cfc9ca2146
MaoZzzzz <602245980@qq.com>
2023-12-09 20:26:48 +0800

    test init

---
+++ src/main/java/com/example/rvptest/RvPTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.rvptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RvPTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RvPTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rvptest/RvPTestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.rvptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RvPTestApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
